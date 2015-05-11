67202d73ac1168f0411b3664771c215d1d0a3973
zaine_mac <username@users.noreply.github.com>
2015-05-11 16:41:37 +1200

    add newField directive method

--- bower_components/src/directives.js
+++ bower_components/src/directives.js
@@ -18,14 +18,14 @@ angular.module('ContactsApp')
 		return {
 			restrict		: 'EA', // restict this object to be used as an element or attribute.
 			templateUrl		: 'views/form-field.html',
-			replace			: true, // HTML from form-fields.html will replace form-field element in new.html, otherwise the HTML will be placed within the form-field element in 					new.html.
+			replace			: true, // HTML from form-fields.html will replace form-field element in new.html, otherwise the HTML will be placed within the form-field element in new.html.
 			scope			: { // determines what attributes on the form-field element will be available in the directive.
 									record		: '=', // changes made within this directive updates the controller - 2 way binding directive.
 									field		: '@', // read only - 1 way binding directive.
 									live		: '@', // read only - 1 way binding directive.
 									required	: '@' // read only - 1 way binding directive.
 							  },
-							link: function($scope, element, attr) { //allows for modifications to be made on this directive when it replaces the current form-field element in 																	new.html.
+							link: function($scope, element, attr) { //allows for modifications to be made on this directive when it replaces the current form-field element in new.html.
 								$scope.$on('record:invalid', function() {
 									$scope[$scope.field].$setDirty();
 								});
@@ -46,9 +46,48 @@ angular.module('ContactsApp')
 								// ng-update function for input field elements.
 								var $saveTimeout;
 								$scope.update = function() {
-									$timeout.cancel(saveTimeout); // Cancel the update after keyup action.
-									saveTimeout = $timeout($scope.blurUpdate, 1000); // update the records after a 1 sec delay, saves to saveTimeout variable.
+									$timeout.cancel($saveTimeout); // Cancel the update after keyup action.
+									$saveTimeout = $timeout($scope.blurUpdate, 1000); // update the records after a 1 sec delay, saves to saveTimeout variable.
 								};
 							}
+		};
+	})
+	
+	.directive('newField', function($filter, FieldTypes) {
+		return {
+			restrict	: 'EA',
+			templateUrl	: 'views/new-field.html',
+			replace		: true,
+			scope		: {
+								record		: '=',
+								live		: '@'
+						  },
+			require	: '^form', // Set the parent form to inherit validation from.
+			link:  function($scope, element, attr, form) {
+						$scope.types = FieldTypes;
+						$scope.field = {};
+
+						$scope.show = function(type) {
+							$scope.field.type = type;
+							$scope.display = true;
+						};
+					
+						$scope.remove = function() {
+							$scope.field = {};
+							$scope.display = false;
+						};
+
+						$scope.add = function() {
+							if(form.newField.$valid) {
+								$scope.record[$filter('camelCase')($scope.field.name)] = [$scope.field.value, $scope.field.type];
+								$scope.remove();
+								if($scope.live !== 'false') {
+									$scope.record.$update(function(updatedRecord) {
+										$scope.record = updatedRecord;
+									});
+								}
+							}
+						};
+					}
 		};
 	});
