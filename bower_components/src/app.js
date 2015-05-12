3f9be4b1f515cfa88cbb8024dfea2068942910ed
zaine_mac <username@users.noreply.github.com>
2015-05-12 19:44:04 +1200

    add otherwise routeProvider redirectTo for login succesful user

--- bower_components/src/app.js
+++ bower_components/src/app.js
@@ -14,6 +14,9 @@ angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
 			.when('/contact/:id', {
 				controller: 'SingleController',
 				templateUrl: 'views/single.html'
+			})
+			.otherwise({
+				redirectTo: '/contacts'
 			});
 			
 		$locationProvider.html5Mode(true);
