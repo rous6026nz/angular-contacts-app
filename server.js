cf5c243319a1ae5f18ce9c79d9674e314d20c7b6
zaine_mac <username@users.noreply.github.com>
2015-05-12 19:52:44 +1200

    require accounts.js and assign to a users variable, add session detection method

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express'),
 	api		= require('./api'),
+	users	= require('./accounts'),
 	app		= express(); // Instantiate the Express application.
 
 app
 	.use(express.static('./bower_components')) // Express middleware - Statically server all file inside bower_components directory.
+	.use(users)
 	.use('/api', api) // Prefix all the routes within the api object with '/api'. 
 	.get('*', function (req, res) {
-		res.sendfile('bower_components/main.html'); // Response method for all responses not caught up until the GET method.
+		if (!req.user) {
+			res.redirect('/login.html');
+		} else {
+			res.sendfile('bower_components/main.html'); // Response method for all responses not caught up until the GET method.
+		}
 	})
 	.listen(3000); // Start the application.
