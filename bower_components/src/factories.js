45b0cfe5632a606fb9dfbc129b85ce8fb3a2ff14
zaine_mac <username@users.noreply.github.com>
2015-05-14 20:25:43 +1200

    add methd to get required fields, filter fields not to be displayed, get stored fields and set what the user wants to store.

--- bower_components/src/factories.js
+++ bower_components/src/factories.js
@@ -3,4 +3,30 @@ angular.module('ContactsApp') // Reference the Angular App.
 		return $resource('/api/contact/:id', { id: '@id' }, {
 			'update': { method: 'PUT' }
 		});
+	})
+	.factory('Fields', function($q, $http Contact) {
+		var url = '/options/displayed_fields',
+			ignore = ['firstName', 'lastName', 'id', 'userId'],
+			allFields = [],
+			deferred = $q.defer(),
+		
+			contacts = Contact.query (function () {
+				contacts.forEach(function (c) {
+					Object.key(c).forEach (function (k) {
+						if (allFields.indexOf(k) < 0 && ignore(k) < 0) allFields.push(k);
+					});
+				});
+				deferred.resolve(allFields);
+			});
+			return {
+				get: function () { // Get what the user has stored.
+					return $http.get(url);
+				},
+				set: function () { // Sets what the user wants to store.
+					return $http.post(url, { fields: newFields });
+				},
+				headers: function () {
+					return.deferred.promise;
+				}
+			};
 	});
